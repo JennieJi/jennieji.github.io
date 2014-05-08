bd0add36d6ccd8a18ca5ec1e54182f41b524316a
Jennie Ji <jennie.ji@hotmail.com>
2014-05-08 21:48:27 +0800

    Update main structure of the page
    newly designed page and starting style the navigation bar

--- config/GruntFile.js
+++ config/GruntFile.js
@@ -7,57 +7,86 @@ module.exports = function(grunt) {
     paths: {
     	scss: '../themes/<%= theme %>/scss',
     	css: '../themes/<%= theme %>/css',
-    	themeJs: '../themes/<%= theme %>/js',
-    	functionalJs: '../scripts'
+      // functionalJs: '../app',
+    	themeJs: '../themes/<%= theme %>/js'
     },
     uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-        mangle: false,
-        compress: false
-      },
-      build: {
+      dist: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
         files: {
+          // '<%= paths.functionalJs %>/app.min.js': [
+          //   '<%= paths.functionalJs %>/lib/*.js',
+          //   '<%= paths.functionalJs %>/dev/*.js'
+          // ],
         	'<%= paths.themeJs %>/theme.min.js': [
         		'<%= paths.themeJs %>/lib/*.js',
-        		'<%= paths.themesJs %>/dev/*.js'
-        	],
-        	'<%= paths.functionalJs %>/app.min.js': [
-        		'<%= paths.functionalJs %>/lib/*.js',
-        		'<%= paths.functionalJs %>/dev/*.js'
+        		'<%= paths.themeJs %>/dev/*.js'
         	]
+        }
+      },
+      dev: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+          beautify: true,
+          compress: false
+        },
+        files: {
+          // '<%= paths.functionalJs %>/app.js': [
+          //   '<%= paths.functionalJs %>/lib/*.js',
+          //   '<%= paths.functionalJs %>/dev/*.js'
+          // ],
+          '<%= paths.themeJs %>/theme.js': [
+            '<%= paths.themeJs %>/lib/*.js',
+            '<%= paths.themeJs %>/dev/*.js'
+          ]
         }
       }
     },
-    sass: {
-    	options: {
-
-    	},
-		files: {
-			'<%= paths.css %>/screen.min.css': '<%= paths.scss %>/screen.scss',
-			'<%= paths.css %>/print.min.css': '<%= paths.scss %>/print.scss'
-		}
+    compass: {
+      dist: {                   // Target
+        options: {
+          sassDir: '<%= paths.scss %>',
+          cssDir: '<%= paths.css %>',
+          environment: 'production'
+        }
+      },
+      dev: {                    // Another target
+        options: {
+          sassDir: '<%= paths.scss %>',
+          cssDir: '<%= paths.css %>',
+          outputStyle: 'expanded'
+        }
+      }
     },
     watch: {
-	  	scripts: {
+	  	dist: {
 		    files: [
-		    	'scripts/**/*.js',
-		    	'themes/<%= theme %>/js/**/*.js'
+          'GruntFile.js',
+		    	// '<%= paths.functionalJs %>/**/*.js',
+		    	'<%= paths.themeJs %>/**/*.js',
+          '<%= paths.scss %>/**/*.scss'
 		    ],
-		    tasks: ['uglify']
-		},
-		styles: {
-			files: 'themes/<%= theme %>/scss/**/*.scss',
-			tasks: ['sass']
-		}
-	}
+		    tasks: ['uglify:dist', 'compass:dist']
+  		},
+  		dev: {
+  			files: [
+          // '<%= paths.functionalJs %>/**/*.js',
+          '<%= paths.themeJs %>/**/*.js',
+          '<%= paths.scss %>/**/*.scss'
+        ],
+        tasks: ['uglify:dev', 'compass:dev']
+  		}
+  	}
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify', 'sass', 'watch']);
+  grunt.registerTask('default', ['uglify:dev', 'compass:dev', 'watch:dev']);
+  grunt.registerTask('product', ['uglify:dist', 'compass:dist', 'watch:dist']);
 
 };
