7816129a40d2e6395e589206e8f3a6e11ac7b4f3
Jennie Ji <jennie.ji@hotmail.com>
2014-06-22 00:42:28 +0800

    Start transfer the web to angular app
    Update the file structures;
    Use bower to load JS libraries;
    Add angular.js;
    Combine bootstrap-theme.css and app.css to reduce file requests;
    Use grunt-contrib-concat to make the debug js file not
    grunt-contrib-uglify any more for keeping the original code.

---
+++ GruntFile.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    paths: {
+    	less: 'theme/less',
+    	css: 'theme/css',
+    	js: 'js',
+      bower: 'bower_components'
+    },
+    less: {
+      prod: {
+        options: {
+          cleancss: true,
+          compress: true
+        },
+        files: {
+          '<%= paths.css %>/bootstrap.min.css': '<%= paths.less %>/bootstrap/bootstrap.less',
+          '<%= paths.css %>/app.min.css': '<%= paths.less %>/app.less'
+        }
+      },
+      dev: {
+        files: {
+          '<%= paths.css %>/bootstrap.css': '<%= paths.less %>/bootstrap/bootstrap.less',
+          '<%= paths.css %>/app.css': '<%= paths.less %>/app.less'
+        }
+      }
+    },
+    concat: {
+      dev: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        src: [
+          '<%= paths.bower %>/angular/angular.min.js',
+          '<%= paths.bower %>/jquery/jquery.min.js',
+          '<%= paths.js %>/*/*.js'
+        ],
+        dest: '<%= paths.js %>/app.js'
+      }
+    },
+    uglify: {
+      prod: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        files: {
+        	'<%= paths.js %>/app.min.js': '<%= concat.dev.dest %>'
+        }
+      }
+    },
+    watch: {
+  		js: {
+  			files: ['<%= concat.dev.src %>'],
+        tasks: ['concat', 'uglify']
+  		},
+      less: {
+        files: [
+          '<%= paths.less %>/**/*.less'
+        ],
+        tasks: ['less:dev']
+      }
+  	}
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat', 'uglify', 'less', 'watch']);
+  grunt.registerTask('product', ['uglify', 'less:prod']);
+  grunt.registerTask('dev', ['concat', 'less:dev']);
+
+};
---
+++ js/angular/controllers/works.js
@@ -0,0 +1,5 @@
+'use strict';
+
+function HelloController($scope) { 
+	$scope.greeting = { text: 'Hello' };
+}
